9596110926a044cc861495cf14fb6a5e011eff12
Rejaur Rahman <rejau@mac.mynetworksettings.com>
2026-02-01 12:49:53 -0500

    Fix use client order for cart

--- frontend/app/cart/page.js
+++ frontend/app/cart/page.js
@@ -1,6 +1,7 @@
+"use client";
+
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
-"use client";
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
